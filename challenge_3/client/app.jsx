aaa018ef8e570914cc1a9e343e51923f70a83c63
morganl92 <morganlinbox@gmail.com>
2019-05-08 20:27:55 -0700

    [feat] create record on checkout and update for each form; separated components for each form

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -18,7 +18,8 @@ class App extends React.Component {
       credit_card: 0,
       expiry_date: 0,
       cvv: 0,
-      billing_zip: 0
+      billing_zip: 0,
+      id: 0
     }
 
     this.handleCheckout = this.handleCheckout.bind(this)
@@ -27,52 +28,64 @@ class App extends React.Component {
     this.handleInput = this.handleInput.bind(this)
   }
 
-  // TODO: POST new record on submit
   handleCheckout(e) {
     e.preventDefault();
+
+    const { name, email, password } = this.state
+
+    axios.post('/api/checkout', {
+      name,
+      email,
+      password
+    })
+      .then(account => this.state.id = account.data.id)
+      .catch(err => console.log(`ERROR POSTING: ${err}`))
 
     this.setState({
       form_one: false,
       form_two: true
     })
-
-    document.getElementById('form1').reset();
   }
 
-  // TODO: find key and perform Axios PUT on submit
   handleNext(e) {
     e.preventDefault();
-    
+
+    const { address_line_1, address_line_2, city, state, zip, phone_number, id } = this.state
+
+    axios.put('/api/checkout', {
+      id: id,
+      update: {
+        address_line_1,
+        address_line_2,
+        city,
+        state,
+        zip,
+        phone_number
+      }
+    })
+      .catch(err => console.log(`ERROR UPDATING: ${err}, ${this.state.id}`))
+
     this.setState({
       form_two: false,
       form_three: true
     })
-
-    document.getElementById('form2').reset();
   }
 
-  // TODO: find key and perform Axios PUT on submit
   handleSubmit(e) {
     e.preventDefault()
 
-    const { name, email, password, address_line_1, address_line_2, city, state, zip, phone_number, credit_card, expiry_date, cvv, billing_zip } = this.state
+    const { credit_card, expiry_date, cvv, billing_zip, id } = this.state
 
-    axios.post('/api/checkout', {
-      name,
-      email,
-      password,
-      address_line_1,
-      address_line_2,
-      city,
-      state,
-      zip,
-      phone_number,
-      credit_card,
-      expiry_date,
-      cvv,
-      billing_zip
+    axios.put('/api/checkout', {
+      id: id,
+      update: {
+        credit_card,
+        expiry_date,
+        cvv,
+        billing_zip
+      }
     })
-      .catch(err => console.log(`ERROR POSTING: ${err}`))
+      .catch(err => console.log(`ERROR UPDATING: ${err}`))
 
     alert("Thank you for your purchase!")
     window.location.reload();
@@ -84,50 +97,64 @@ class App extends React.Component {
     })
   }
 
-  // TODO: create additional components to handle different forms
   render() {
     if (this.state.form_one) {
       return (
         <div>
-          <h1>Checkout Form</h1>
-          <form id="form1" onSubmit={this.handleCheckout}>
-            Name: <input type="text" id="name" onChange={this.handleInput} /> <br />
-            Email: <input type="text" id="email" onChange={this.handleInput} /> <br />
-            Password: <input type="password" id="password" onChange={this.handleInput} /> <br />
-            <input type="submit" value="Checkout" />
-          </form>
+          <h1>Multistep Checkout Experience</h1><br />
+          <h2>Account Form</h2>
+          <AccountForm handleCheckout={this.handleCheckout} handleInput={this.handleInput} />
         </div>
       )
     } else if (this.state.form_two) {
       return (
         <div>
-          <h1>Address Form</h1>
-          <form id="form2" onSubmit={this.handleNext}>
-            Address Line 1: <input type="text" id="address_line_1" onChange={this.handleInput} /> <br />
-            Address Line 2: <input type="text" id="address_line_2" onChange={this.handleInput} /> <br />
-            City: <input type="text" id="city" onChange={this.handleInput} /> <br />
-            State: <input type="text" id="state" onChange={this.handleInput} /> <br />
-            Zip: <input type="number" id="zip" onChange={this.handleInput} /> <br />
-            Phone Number: <input type="number" id="phone_number" onChange={this.handleInput} /> <br />
-            <input type="submit" value="Next" />
-          </form>
+          <h1>Multistep Checkout Experience</h1><br />
+          <h2>Address Form</h2>
+          <AddressForm handleNext={this.handleNext} handleInput={this.handleInput} />
         </div>
       )
     } else if (this.state.form_three) {
       return (
         <div>
-          <h1>Credit Card Form</h1>
-          <form id="form3" onSubmit={this.handleSubmit}>
-            Credit Card: <input type="number" id="credit_card" onChange={this.handleInput} /> <br />
-            Expiry Date: <input type="number" id="expiry_date" onChange={this.handleInput} /> <br />
-            CVV: <input type="number" id="cvv" onChange={this.handleInput} /> <br />
-            Billing Zip: <input type="number" id="billing_zip" onChange={this.handleInput} /> <br />
-            <input type="submit" />
-          </form>
+          <h1>Multistep Checkout Experience</h1><br />
+          <h2>Payment Form</h2>
+          <PaymentForm handleSubmit={this.handleSubmit} handleInput={this.handleInput} />
         </div>
       )
     }
   }
 }
+
+const AccountForm = (props) => (
+  <form id="accountForm" onSubmit={props.handleCheckout}>
+    Name: <input type="text" id="name" onChange={props.handleInput} /> <br />
+    Email: <input type="text" id="email" onChange={props.handleInput} /> <br />
+    Password: <input type="password" id="password" onChange={props.handleInput} /> <br />
+    <input type="submit" value="Checkout" />
+  </form>
+)
+
+const AddressForm = (props) => (
+  <form id="addressForm" onSubmit={props.handleNext}>
+    Address Line 1: <input type="text" id="address_line_1" onChange={props.handleInput} /> <br />
+    Address Line 2: <input type="text" id="address_line_2" onChange={props.handleInput} /> <br />
+    City: <input type="text" id="city" onChange={props.handleInput} /> <br />
+    State: <input type="text" id="state" onChange={props.handleInput} /> <br />
+    Zip: <input type="number" id="zip" onChange={props.handleInput} /> <br />
+    Phone Number: <input type="number" id="phone_number" onChange={props.handleInput} /> <br />
+    <input type="submit" value="Next" />
+  </form>
+)
+
+const PaymentForm = (props) => (
+  <form id="paymentForm" onSubmit={props.handleSubmit}>
+    Credit Card: <input type="number" id="credit_card" onChange={props.handleInput} /> <br />
+    Expiry Date: <input type="number" id="expiry_date" onChange={props.handleInput} /> <br />
+    CVV: <input type="number" id="cvv" onChange={props.handleInput} /> <br />
+    Billing Zip: <input type="number" id="billing_zip" onChange={props.handleInput} /> <br />
+    <input type="submit" />
+  </form>
+)
 
 ReactDOM.render(<App />, document.getElementById('app'));
--- challenge_3/server/server.js
+++ challenge_3/server/server.js
@@ -17,14 +17,16 @@ app.get('/api/checkout', (req, res) => {
     .catch(err => res.status(404).send(`GET ERROR: ${err}`))
 })
 
-/*
-  TODO: create update method
-*/
-
 app.post('/api/checkout', (req, res) => {
   User.create(req.body)
-    .then(() => res.status(201).send('New Account Created!'))
+    .then(data => res.status(201).send(data))
     .catch(err => res.status(404).send(`POST ERROR: ${err}`))
+})
+
+app.put('/api/checkout', (req, res) => {
+  User.update(req.body.update, { where: { id: req.body.id } })
+    .then(() => res.status(201).send('Record updated!'))
+    .catch(err => res.status(404).send(`UPDATE ERROR: ${err}`))
 })
 
 app.listen(port, () => console.log('Server is listening on port:', port))
